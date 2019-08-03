19a827f1b55be51d8d058141bb89e3cb0aa37008
Patrick McElhaney <pmcelhaney@jackhenry.com>
2019-08-03 19:29:15 -0400

    Nicer CLI table

--- cli.js
+++ cli.js
@@ -1,18 +1,21 @@
 const Boolio = require("./Boolio");
-const AsciiTable = require("ascii-table");
+const table = require("table").table;
 
 const expression = process.argv[2];
 
 const truthTable = new Boolio(expression).truthTable();
 
-const asciiTable = AsciiTable.factory({
-  heading: [...truthTable.atoms, " = "],
-  rows: truthTable.rows.map(cols => cols.map(cell => (cell ? "T" : "F")))
-});
-
-for (let i = 0; i <= truthTable.atoms.length; i++) {
-  asciiTable.setAlign(i, AsciiTable.CENTER);
-}
-
-console.log(expression);
-console.log(asciiTable.toString());
+console.log(
+  table(
+    [
+      [...truthTable.atoms, "="],
+      ...truthTable.rows.map(row => row.map(cell => (cell ? "T" : "F")))
+    ],
+    {
+      columns: new Array(4).fill({
+        width: 10,
+        alignment: "center"
+      })
+    }
+  )
+);
