410534172d003daee820888b8962079292e7ab6f
Patrick McElhaney <pmcelhaney@jackhenry.com>
2019-11-10 12:33:20 -0500

    The Quine-McClusky impementation is complete. Yay

--- quineMcClusky.js
+++ quineMcClusky.js
@@ -3,63 +3,121 @@
 // map essentialExpression to AND-expressions and join them with OR
 
 function quineMcClusky(minTerms) {
-  const setOfGroups = initializeSetOfGroups(minTerms[0].length); //  stairstep array of empty arrays (..., [ [],[],[] ], [ [],[] ], [ [] ])
+  const setOfGroups = initializeSetOfGroups(minTerms[0].length + 1);
+  console.log(setOfGroups[0].length);
   placePrimeImplicantsInCountOfOneGroups(
-    primeImplicantsFormMinterms(minTerms),
+    primeImplicantsFromMinterms(minTerms),
     setOfGroups[0]
   );
-  const essentialPIs = [];
   for (let iteration = 0; iteration < setOfGroups.length - 1; iteration++) {
-    for (
-      let countOfOnes = 0;
-      i < setOfGroups[iteration].length - 1;
-      countOfOnes++
-    ) {
-      for (a in setOfGroups[iteration][countOfOnes]) {
-        for (b in setOfGroups[iteration][countOfOnes + 1]) {
+    for (let index = 0; index < setOfGroups[iteration].length - 1; index++) {
+      for (a of setOfGroups[iteration][index]) {
+        for (b of setOfGroups[iteration][index + 1]) {
           if (canCombine(a, b)) {
             a.isUsed = true;
             b.isUsed = true;
-            setOfGroups[iteration + 1][countOfOnes].push(combine(a, b));
+            const c = combine(a, b);
+            const targetGroup = setOfGroups[iteration + 1][index];
+            if (!targetGroup.some(x => x.value === c.value)) {
+              setOfGroups[iteration + 1][index].push(combine(a, b));
+            }
           }
         }
       }
     }
   }
-  // essentialPIs = all unused in setOfGroups
-  // sort essentialPIs by count of minterms descending
-  // for each essentialPI (working backwards)
-  //     if all of its minterms can be covered by other remaining PIs
-  //         remove it
+  const essentialPIs = setOfGroups
+    .flat(2)
+    .filter(x => !x.isUsed)
+    .sort((a, b) => b.minterms.size - a.minterms.size);
 
-  return essentialPIs.map(pi => pi.expression);
+  for (let i = essentialPIs.length - 1; i >= 0; i--) {
+    const thisPI = essentialPIs[i];
+    const mintermsInOtherPIs = essentialPIs
+      .filter(pi => pi.value !== thisPI.value)
+      .map(pi => [...pi.minterms])
+      .flat();
+    if (
+      [...thisPI.minterms].every(minterm =>
+        mintermsInOtherPIs.includes(minterm)
+      )
+    ) {
+      essentialPIs.splice(i, 1);
+    }
+  }
+
+  return essentialPIs.map(pi => pi.value);
 }
 
 function initializeSetOfGroups(count) {
-  // return stairstep array of empty arrays (..., [ [],[],[] ], [ [],[] ], [ [] ])
+  const result = [];
+  for (let size = 1; size <= count; size++) {
+    result.unshift(new Array(size).fill(null).map(x => []));
+  }
+
+  return result;
 }
 
 function primeImplicantsFromMinterms(minterms) {
   return minterms.map(minterm => ({
-    expression: minterm,
-    minterms: [minterm],
+    value: minterm,
+    minterms: new Set([minterm]),
     isUsed: false
   }));
 }
 
 function placePrimeImplicantsInCountOfOneGroups(primeImplicants, groups) {
   primeImplicants.forEach(pi => {
-    const countOfOnes = pi.expression.replace(/[^1]/, "").length;
+    const countOfOnes = pi.value.replace(/[^1]/g, "").length;
     groups[countOfOnes].push(pi);
   });
 }
 
-function canCombine(a, b) {}
+function canCombine(primeImplicantA, primeImplicantB) {
+  const a = primeImplicantA.value;
+  const b = primeImplicantB.value;
+  let alreadyFoundADifference = false;
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] === b[i]) {
+      continue;
+    }
+    if (a[i] === "_" || b[i] === "_") {
+      return false;
+    }
+    if (alreadyFoundADifference) {
+      return false;
+    }
+    alreadyFoundADifference = true;
+  }
+  return true;
+}
 
-function combine(a, b) {}
+function combine(primeImplicantA, primeImplicantB) {
+  const a = primeImplicantA.value;
+  const b = primeImplicantB.value;
+  let c = "";
 
-// expression: e.g. "1001" or "1_01"
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] === b[i]) {
+      c += a[i];
+    } else {
+      c += "_";
+    }
+  }
+  return {
+    value: c,
+    isUsed: false,
+    minterms: new Set([
+      ...primeImplicantA.minterms,
+      ...primeImplicantB.minterms
+    ])
+  };
+}
+
+// value: e.g. "1001" or "1_01"
 // PrimeImplicant
 // - isUsed (boolean)
 // - minTerms (array of expressions that do not have underscores)
-// - expression
+// - value (expression that can have underscores)
+
+// quineMcClusky(['10100', '10101', '10110', '10111', '11001', '11111', '11011']) => ["110_1", "11_11", "101__"]
